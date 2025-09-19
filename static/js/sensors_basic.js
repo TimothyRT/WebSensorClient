cb43afc5ecef16874cca9574268d4cd842a0528d
TimothyRT <timothyrtzion@gmail.com>
2025-09-19 23:32:59 +0700

    Added linear acceleration sensor support

--- static/js/sensors_basic.js
+++ static/js/sensors_basic.js
@@ -8,15 +8,30 @@ else {
   const gyroscopeLabelY = document.querySelector("#gyroscope-y");
   const gyroscopeLabelZ = document.querySelector("#gyroscope-z");
 
-  console.log(gyroscopeLabelX);
-  console.log(gyroscopeLabelY);
-  console.log(gyroscopeLabelZ);
-
   gyroscope.addEventListener("reading", (e) => {
-    gyroscopeLabelX.innerText = `Angular velocity along the X-axis ${gyroscope.x}`;
-    gyroscopeLabelY.innerText = `Angular velocity along the Y-axis ${gyroscope.y}`;
-    gyroscopeLabelZ.innerText = `Angular velocity along the Z-axis ${gyroscope.z}`;
+    gyroscopeLabelX.innerText = `Angular velocity along the X-axis = ${gyroscope.x}`;
+    gyroscopeLabelY.innerText = `Angular velocity along the Y-axis = ${gyroscope.y}`;
+    gyroscopeLabelZ.innerText = `Angular velocity along the Z-axis = ${gyroscope.z}`;
   });
 
   gyroscope.start();
+}
+
+if (typeof LinearAccelerationSensor === "undefined") {
+  alert("LinearAccelerationSensor API not supported on this device/browser.")
+}
+else {
+  const acceleration = new LinearAccelerationSensor({ frequency: 60 });
+
+  const accelerationLabelX = document.querySelector("#acceleration-x");
+  const accelerationLabelY = document.querySelector("#acceleration-y");
+  const accelerationLabelZ = document.querySelector("#acceleration-z");
+
+  acceleration.addEventListener("reading", (e) => {
+    accelerationLabelX.innerText = `Acceleration along the X-axis = ${gyroscope.x}`;
+    accelerationLabelY.innerText = `Acceleration along the Y-axis = ${gyroscope.y}`;
+    accelerationLabelZ.innerText = `Acceleration along the Z-axis = ${gyroscope.z}`;
+  });
+
+  acceleration.start();
 }
