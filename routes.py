8c8fa1c010cc084f1b67246a347f1e0801519f2e
TimothyRT <timothyrtzion@gmail.com>
2025-09-20 17:10:45 +0700

    apply_csp

--- routes.py
+++ routes.py
@@ -7,3 +7,12 @@ main_bp = Blueprint("main", __name__, url_prefix="/")
 @main_bp.route("/")
 def sensors_basic():
     return render_template("sensors_basic.html")
+
+
+@main_bp.after_request
+def apply_csp(response):
+    response.headers["Content-Security-Policy"] = (
+        "default-src 'self'; "
+        "connect-src 'self' ws: wss: https:;"
+    )
+    return response
