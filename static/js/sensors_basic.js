894baa1f0a37d0f48e6d0f8242c220601df61401
TimothyRT <timothyrtzion@gmail.com>
2025-09-20 17:15:27 +0700

    port

--- static/js/sensors_basic.js
+++ static/js/sensors_basic.js
@@ -4,7 +4,7 @@ const ipAddress = urlParams.get('ip');
 if (ipAddress === null) {
   alert("The Godot process's IP address was not provided in URL param!");
 } else {
-  const ws = new WebSocket(`wss://${ipAddress}`);
+  const ws = new WebSocket(`wss://${ipAddress}:9080`);
   console.log(`[WebSocket] Attempted to establish WebSocket connection; IP address = ${ipAddress}`)
 
   ws.onopen = () => {
